71f70203492d3422c6a9f015861c4177fa8af18a
Eric Oliver <eroliver27@hotmail.com>
2021-12-08 03:19:19 -0500

    Add project files.

---
+++ WorkingWithFiles/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+
+namespace WorkingWithFiles
+{
+    public class Class1
+    {
+        
+        IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                // The file name will contain the full path, so only check the end of it
+                if (file.EndsWith("sales.json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+
+            return salesFiles;
+        }
+
+        public void GetFiles()
+        {
+            var salesFiles = FindFiles("stores");
+
+            foreach (var file in salesFiles)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+    }
+}
