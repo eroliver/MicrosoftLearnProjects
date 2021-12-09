75939c3cf062f007d991f96077d2be3357bca0c7
Eric Oliver <eroliver27@hotmail.com>
2021-12-09 08:06:11 -0500

    pushing to finish remotely

--- WorkingFiles/Program.cs
+++ WorkingFiles/Program.cs
@@ -2,36 +2,39 @@
 using System.IO;
 using System.Collections.Generic;
 
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var storesDirectory = Path.Combine(currentDirectory, "stores");
+        var salesFiles = FindFiles(storesDirectory);
+
+        foreach (var file in salesFiles)
         {
-            var salesFiles = FindFiles("stores");
-
-            foreach (var file in salesFiles)
-            {
-                Console.WriteLine(file);
-            }
-
-            IEnumerable<string> FindFiles(string folderName)
-            {
-                List<string> salesFiles = new List<string>();
-
-                var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
-
-                foreach (var file in foundFiles)
-                {
-                    // The file name will contain the full path, so only check the end of it
-                    if (file.EndsWith("sales.json"))
-                    {
-                        salesFiles.Add(file);
-                    }
-                }
-
-                return salesFiles;
-            }
-
-
+            Console.WriteLine(file);
         }
+
+        IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                var extension = Path.GetExtension(file);
+                // The file name will contain the full path, so only check the end of it
+                if (file.EndsWith("sales.json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+
+            return salesFiles;
+        }
+
+
     }
+}
 
