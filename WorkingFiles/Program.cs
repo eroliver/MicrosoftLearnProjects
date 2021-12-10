3b8a820dbf50689cfbd166a6eb03c6f869554c15
eroliver <eroliver27@hotmail.com>
2021-12-10 15:46:42 -0500

    Need to update C# version to be able to use static external functions. Otherwise, this exercise is finished.

--- WorkingFiles/Program.cs
+++ WorkingFiles/Program.cs
@@ -1,37 +1,63 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json; 
 
-    class Program
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var storesDirectory = Path.Combine(currentDirectory, "stores");
+        var salesTotalDirectory = Path.Combine(currentDirectory, "salesTotalDirectory");
+        Directory.CreateDirectory(salesTotalDirectory);
+
+        var salesFiles = FindFiles(storesDirectory);
+
+        var salesTotal = CalculateSalesTotal(salesFiles);
+
+        File.AppendAllText(Path.Combine(salesTotalDirectory, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+
+        IEnumerable<string> FindFiles(string folderName)
         {
-            var salesFiles = FindFiles("stores");
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+            
+            foreach (var file in foundFiles)
+            {
+                var extension = Path.GetExtension(file);
+                if (extension == ".json")
+                {
+                    salesFiles.Add(file);
+                }
+            }
+
+            return salesFiles;
+        }
+
+        static extern record SalesData (double Total);
+
+        static double CalculateSalesTotal(IEnumerable<string> salesFiles)
+        {
+            double salesTotal = 0;
 
             foreach (var file in salesFiles)
-            {
-                Console.WriteLine(file);
+            {      
+                // Read the contents of the file
+                string salesJson = File.ReadAllText(file);
+
+                // Parse the contents as JSON
+                SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+                // Add the amount found in the Total field to the salesTotal variable
+                salesTotal += data?.Total ?? 0;
             }
 
-            IEnumerable<string> FindFiles(string folderName)
-            {
-                List<string> salesFiles = new List<string>();
-
-                var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
-
-                foreach (var file in foundFiles)
-                {
-                    // The file name will contain the full path, so only check the end of it
-                    if (file.EndsWith("sales.json"))
-                    {
-                        salesFiles.Add(file);
-                    }
-                }
-
-                return salesFiles;
-            }
-
-
+            return salesTotal;
         }
     }
+}
 
